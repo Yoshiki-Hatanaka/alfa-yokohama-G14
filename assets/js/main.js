319d6db20fdcb41ba93618e8f550206557102893
Yoshiki-Hatanaka <89884621+Yoshiki-Hatanaka@users.noreply.github.com>
2021-09-13 20:54:08 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,9 +29,11 @@ const app = Vue.createApp({
           q1: 'ううう',
           // q2: 'かかか',
           // q3: 'ききき',
-        }
+        },
         stage4: {
-          q1:
+          q1: 'えええ',
+          // q2: 'くくく',
+          // q3: 'けけけ',
       },
 
       /* それぞれの問題が正解かどうか
@@ -50,6 +52,11 @@ const app = Vue.createApp({
           false, // 3-1
           // false, // 3-2
           // false, // 3-3
+        ],
+        stage4: [
+          false, // 4-1
+          // false, // 4-2
+          // false, // 4-3
         ]
       },
 
@@ -58,6 +65,7 @@ const app = Vue.createApp({
         stage1: false,
         stage2: false,
         stage3: false,
+        stage4: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -66,6 +74,7 @@ const app = Vue.createApp({
       next: {
         stage1: false,
         stage2: false,
+        stage3: false,
       },
     }
   },
